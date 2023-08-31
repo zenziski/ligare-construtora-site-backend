6f5227cba70c4cc3c9423904e1d661b1973a4ec1
zenziski <matheus.zenziski@gmail.com>
2023-08-31 19:39:56 -0300

    fix pagination

--- resources/upload/get.ts
+++ resources/upload/get.ts
@@ -4,14 +4,21 @@ module.exports = (app: any) => ({
     verb: "get",
     route: '/',
     handler: async (req: any, res: Response) => {
+        const perPage = 21;
+        const page = Math.max(0, req.query.page);
+        console.log(req.params.page);
         const { File } = app.models;
-        const files = await File.find({}).lean();
+        const totalFiles = await File.find({}).lean()
+        const files = await File.find({})
+            .limit(perPage)
+            .skip(perPage * page)
+            .lean();
         let mappedFiles = files.map((file: any) => {
             return {
                 ...file,
                 location: `${process.env.URL_BACKEND}/${file.location}`
             }
         })
-        return res.status(200).json({ files: mappedFiles })
+        return res.status(200).json({ files: mappedFiles, totalFiles: totalFiles.length })
     }
 });
