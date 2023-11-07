857725970a3f67498653d453a553adedf04a8a9a
Rodrigo Onesko Dias <51219687+Rodrigonesko@users.noreply.github.com>
2023-11-07 12:25:03 -0300

    adicionando email no contato

--- models/contact.ts
+++ models/contact.ts
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
 var schema = mongoose.Schema({
     name: String,
     phone: String,
+    email: String,
     description: String,
 }, { timestamps: true });
 
--- resources/contact/post.ts
+++ resources/contact/post.ts
@@ -11,6 +11,7 @@ module.exports = (app: any) => ({
             await Contact.create({
                 name: body.name,
                 phone: body.phone,
+                email: body.email,
                 description: body.description
             })
         }
